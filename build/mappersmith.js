aec322e768c6d02b0de35f39dcb9c619e97fd8f7
tulios <ornelas.tulio@gmail.com>
2014-12-16 19:39:02 -0200

    better place for jquery check

--- build/mappersmith.js
+++ build/mappersmith.js
@@ -77,6 +77,13 @@ var Utils = require('../utils');
 var Gateway = require('../gateway');
 
 var JQueryGateway = function() {
+  if (window.jQuery === undefined) {
+    throw new Utils.Exception(
+      'JQueryGateway requires jQuery but it was not found! ' +
+      'Change the gateway implementation or add jQuery on the page'
+    );
+  }
+
   return Gateway.apply(this, arguments);
 }
 
@@ -85,13 +92,6 @@ JQueryGateway.prototype = Utils.extend({}, Gateway.prototype, {
   get: function() {
     var defaults = {dataType: "json", url: this.url};
     var config = Utils.extend(defaults, this.opts);
-
-    if (window.jQuery === undefined) {
-      throw new Utils.Exception(
-        'JQueryGateway#get requires jQuery but it was not found! ' +
-        'Change the gateway implementation or add jQuery on the page'
-      );
-    }
 
     jQuery.ajax(config).
     done(function() { this.successCallback.apply(this, arguments) }.bind(this)).
