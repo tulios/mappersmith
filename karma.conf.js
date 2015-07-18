1b92c7650db5b2c0a12bb94ee95cb594d33211ce
Túlio Ornelas <tulio.ornelas@klarna.com>
2015-07-18 11:41:14 +0200

    configured travisCI

--- karma.conf.js
+++ karma.conf.js
@@ -59,6 +59,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS2'], //'Firefox', 'Chrome'],
+    browserNoActivityTimeout: 30000,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
