2fdd77c41bfcdfacefc93dc297c1a1ff94dec65d
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-06-17 14:29:57 +0200

    updated compiled versions

--- build/mappersmith.js
+++ build/mappersmith.js
@@ -1,6 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Mappersmith = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*!
- * Mappersmith 0.13.2
+ * Mappersmith 0.13.3
  * https://github.com/tulios/mappersmith
  */
 module.exports = {
@@ -75,6 +75,7 @@ var Gateway = function(args) {
   this.method = args.method;
   this.body = args.body;
   this.processor = args.processor;
+  this.beforeSend = args.beforeSend;
   this.opts = args.opts || {};
 
   this.timeStart = null;
@@ -90,6 +91,7 @@ Gateway.prototype = {
 
   call: function() {
     this.timeStart = Utils.performanceNow();
+    if (this.beforeSend) this.beforeSend(this);
 
     if (Env.USE_FIXTURES && Env.Fixture) {
       this.callWithFixture();
@@ -434,7 +436,7 @@ var VanillaGateway = CreateGateway({
       if (/^content-type$/i.test(headerName)) contentType = headers[headerName];
     });
 
-    if (!contentType) contentType = 'application/x-www-form-urlencoded; charset=UTF-8;';
+    if (!contentType) contentType = 'application/x-www-form-urlencoded;charset=utf-8';
     request.setRequestHeader('Content-Type', contentType);
   },
 
@@ -598,6 +600,7 @@ Mapper.prototype = {
         method: descriptor.method,
 
         processor: descriptor.processor || rules.processor,
+        beforeSend: descriptor.beforeSend || rules.beforeSend,
         opts: opts
       });
 
