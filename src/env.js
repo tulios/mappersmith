50bb6c3675d4167e494fb99bb31d9d77584649ed
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-10-30 19:14:19 +0100

    cleanup v1

--- src/env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  USE_FIXTURES: false,
-  USE_PROMISES: false,
-  Fixture: null,
-  Promise: typeof Promise === 'function' ? Promise : null
-}
--- test/node/node-gateways-impl-test.js
+++
@@ -1,9 +0,0 @@
-require('./test-helper');
-var gatewayImplSpecFor = require('../helpers/shared-gateway-impl-spec');
-var Mappersmith = require('../../node');
-
-describe('gateway impl', function() {
-
-  gatewayImplSpecFor('NodeVanillaGateway', Mappersmith.node.NodeVanillaGateway);
-
-});
