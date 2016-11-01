c7154f8e4bf8438ed9b8e08b92151c86bb8169bd
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-11-01 18:45:53 +0100

    tests for utils#performanceNow on node

--- spec/node/utils-spec.js
+++ spec/node/utils-spec.js
@@ -3,7 +3,43 @@ import {
 } from 'src/utils'
 
 describe('#performanceNow', () => {
-  it('works', () => {
-    expect(true).toEqual(true)
+  it('initially gives a near zero (< 20 ms) time', () => {
+    expect(performanceNow() < 20).toEqual(true)
+  })
+
+  it('gives a positive time', () => {
+    expect(performanceNow() > 0).toEqual(true)
+  })
+
+  it('two subsequent calls returns an increasing number', () => {
+    expect(performanceNow() < performanceNow()).toEqual(true)
+  })
+
+  it('has less than 10 microseconds overhead', () => {
+    expect(Math.abs(performanceNow() - performanceNow()) < 0.010).toEqual(true)
+  })
+
+  it('shows that at least 990 ms has passed after a timeout of 1 second', (done) => {
+    const a = performanceNow()
+    setTimeout(() => {
+      const diff = performanceNow() - a
+      if (diff < 990) {
+        return done.fail(`Diff (${diff}) lower than 990.`)
+      }
+
+      done()
+    }, 1000)
+  })
+
+  it('shows that not more than 1020 ms has passed after a timeout of 1 second', (done) => {
+    const a = performanceNow()
+    setTimeout(() => {
+      const diff = performanceNow() - a
+      if (diff > 1020) {
+        return done.fail(`Diff (${diff}) higher than 1020.`)
+      }
+
+      done()
+    }, 1000)
   })
 })
