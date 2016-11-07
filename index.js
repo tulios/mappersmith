665bdd6ab2255486454d23d199c96c6063dbba9c
Túlio Ornelas <tulio.ornelas@klarna.com>
2016-11-07 21:57:11 +0100

    load only from precompiled source

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-var lib, defaultGateway
-
-try {
-  // Try loading the compiled code.
-  lib = require('./lib')
-} catch (e) {
-  // If the compiled code is not available, load from source.
-  try {
-    require('babel-register')({ only: /(mappersmith\/src)/ })
-    lib = require('./src')
-  } catch (e) {
-    throw e
-  }
-}
+// Load compiled code
+var lib = require('./lib')
+var defaultGateway
 
 if (typeof XMLHttpRequest !== 'undefined') {
   // For browsers use XHR adapter
-  defaultGateway = require('./src/gateway/xhr').default
+  defaultGateway = require('./lib/gateway/xhr').default
 } else if (typeof process !== 'undefined') {
   // For node use HTTP adapter
   defaultGateway = require('./lib/gateway/http').default
