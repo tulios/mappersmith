bf6b2a4a6430bb5a6edd9ceeec0daad947e811dd
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-03-02 14:48:47 +0100

    add fetch gateway

--- src/mappersmith.js
+++ src/mappersmith.js
@@ -2,6 +2,8 @@ import ClientBuilder from './client-builder'
 
 export const configs = {
   Promise: typeof Promise === 'function' ? Promise : null,
+  fetch: typeof fetch === 'function' ? fetch : null,
+
   /**
    * Gateway implementation, it defaults to "lib/gateway/xhr" for browsers and
    * "lib/gateway/http" for node
@@ -20,17 +22,35 @@ export const configs = {
        * Indicates whether or not cross-site Access-Control requests should be made using credentials
        * such as cookies, authorization headers or TLS client certificates.
        * Setting withCredentials has no effect on same-site requests
+       *
        * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/withCredentials
+       *
        * @default false
        */
       withCredentials: false,
 
       /**
-       * @callback to add aditional configurations to the XMLHttpRequest object.
+       * To add aditional configurations to the XMLHttpRequest object.
        * @param {XMLHttpRequest} xhr
        * @default null
        */
       configure: null
+    },
+
+    Fetch: {
+      /**
+       * Indicates whether the user agent should send cookies from the other domain in the case of cross-origin
+       * requests. This is similar to XHR’s withCredentials flag, but with three available values (instead of two):
+       *
+       * "omit": Never send cookies.
+       * "same-origin": Only send cookies if the URL is on the same origin as the calling script.
+       * "include": Always send cookies, even for cross-origin calls.
+       *
+       * https://developer.mozilla.org/en-US/docs/Web/API/Request/credentials
+       *
+       * @default "omit"
+       */
+      credentials: 'omit'
     }
   }
 }
