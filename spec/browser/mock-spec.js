a2d5ad1ed78d727d583b2a041b3db9fce6a8e19e
Túlio Ornelas <tulio.ornelas@klarna.com>
2017-04-07 16:06:06 +0200

    Add a list of matching functions to the test lib

--- spec/browser/mock-spec.js
+++ spec/browser/mock-spec.js
@@ -7,7 +7,8 @@ import {
   install as installMock,
   uninstall as uninstallMock,
   mockClient,
-  mockRequest
+  mockRequest,
+  m
 } from 'src/test'
 
 describe('Test lib', () => {
@@ -406,6 +407,40 @@ describe('Test lib', () => {
           const error = response.rawData ? response.rawData() : response
           done.fail(`test failed with promise error: ${error}`)
         })
+    })
+  })
+
+  describe('match functions', () => {
+    describe('#stringMatching', () => {
+      it('returns true when it matches', () => {
+        const fn = m.stringMatching(/^valid$/)
+        expect(fn('random string')).toEqual(false)
+        expect(fn('valid')).toEqual(true)
+      })
+
+      it('throws an exception if the argument is not a regexp', () => {
+        expect(() => m.stringMatching({}))
+          .toThrowError('[Mappersmith Test] "stringMatching" received an invalid regexp ([object Object])')
+      })
+    })
+
+    describe('#stringContaining', () => {
+      it('returns true when it includes the substring', () => {
+        const fn = m.stringContaining('valid')
+        expect(fn('random string')).toEqual(false)
+        expect(fn('somevalidstuff')).toEqual(true)
+      })
+
+      it('throws an exception if the argument is not a string', () => {
+        expect(() => m.stringContaining({}))
+          .toThrowError('[Mappersmith Test] "stringContaining" received an invalid string ([object Object])')
+      })
+    })
+
+    describe('#anything', () => {
+      it('always returns true', () => {
+        expect(m.anything()()).toEqual(true)
+      })
     })
   })
 })
