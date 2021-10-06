39a77d8315bf5a45222bdc2995ae9e34855c87e2
Mathias Klippinge <mathias.klippinge@gmail.com>
2021-10-06 19:06:40 +0200

    fix ts io directories

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,6 @@
 set -euv
 
 sh -c "NODE_ENV=production npm run build"
-cp -r LICENSE README.md package.json typings lib/
+cp -r LICENSE README.md package.json typings built lib/
 cp examples/basic.js lib/example.js
 sh -c "cd lib; npm publish"
