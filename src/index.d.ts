777969e3e5dc32d713040bb9839c897a08c3ea33
Mathias Klippinge <mathias.klippinge@gmail.com>
2022-02-18 14:29:15 +0100

    Emit .d.ts to lib instead of typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,38 @@
-import * as lib from './mappersmith'
-export default lib
+import type { GlobalConfigs, ManifestOptions, ResourceTypeConstraint } from './manifest'
+export type { GlobalConfigs, ManifestOptions, ResourceTypeConstraint }
+export type { Request } from './request'
+export type { Headers, Params as Parameters } from './types'
+export type { Gateway } from './gateway'
+export type { XHR as XhrGateway } from './gateway/xhr'
+export type { HTTP as HTTPGateway } from './gateway/http'
+export type { Fetch as FetchGateway } from './gateway/fetch'
+export type { Mock as MockGateway } from './gateway/mock'
+export type {
+  HTTPRequestParams,
+  HTTPGatewayConfiguration,
+  GatewayConfiguration,
+} from './gateway/types'
+export { Response, ParsedJSON } from './response'
+export type {
+  AbortFn,
+  Authorization,
+  Context,
+  Middleware,
+  MiddlewareDescriptor,
+  MiddlewareParams,
+  RenewFn,
+  RequestGetter,
+  ResponseGetter,
+} from './middleware'
+export type { AsyncFunction, AsyncFunctions, Client } from './client-builder'
+/**
+ * @deprecated, use ManifestOptions instead
+ */
+export type Options<Resources extends ResourceTypeConstraint> = ManifestOptions<Resources>
+
+/**
+ * @deprecated, use GlobalConfigs instead
+ */
+export type Configuration = GlobalConfigs
+
+export { default, version, configs, setContext } from './mappersmith'
