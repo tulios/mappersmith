2d65531030068e693084e327032984837b386877
Mathias Klippinge <mathias.klippinge@gmail.com>
2022-02-18 14:29:37 +0100

    Drop typings/ folder

--- typings/middleware/global-error-handler.d.ts
+++
@@ -1,9 +0,0 @@
-import { Middleware } from './index'
-import { Response } from '../response'
-
-declare const GlobalErrorHandler: Middleware
-
-export type ErrorHandlerMiddlewareCallback = (response: Response) => boolean
-
-export function setErrorHandler(fn: ErrorHandlerMiddlewareCallback): void
-export default GlobalErrorHandler
