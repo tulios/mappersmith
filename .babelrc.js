40314ac1809531f33d626603c80e185d1fca9e64
Mathias Klippinge <mathias.klippinge@gmail.com>
2022-03-16 10:35:07 +0100

    Ensure references to `regeneratorRuntime` is not part of compiled bundle (#304)

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        exclude: ['@babel/plugin-transform-regenerator'],
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     '@babel/proposal-object-rest-spread',
