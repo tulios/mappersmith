f0491a2787347e9b8f371205b1d15a3102e3f84a
Mathias Klippinge <mathias.klippinge@gmail.com>
2023-09-12 12:51:33 +0200

    Change build step

--- scripts/build-project.sh
+++ scripts/build-project.sh
@@ -2,5 +2,5 @@
 set -euv
 
 sh -c "NODE_ENV=production yarn build:clean"
-cp -r LICENSE README.md package.json src/version.json lib/
-cp examples/basic.js lib/example.js
+# cp -r LICENSE README.md package.json src/version.json lib/
+# cp examples/basic.js lib/example.js
