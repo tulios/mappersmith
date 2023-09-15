197b28da9a886d1e1f28145f30a5fe2d33afc614
Mathias Klippinge <mathias.klippinge@gmail.com>
2023-09-15 12:40:10 +0200

    Remove some webpack stuff

--- typings/pnp-webpack-plugin/index.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-unused-modules */
-declare module 'pnp-webpack-plugin' {
-  import webpack from 'webpack'
-
-  export default class PnpWebpackPlugin implements webpack.WebpackPluginInstance {
-    static moduleLoader(module: NodeModule): any
-    apply(): void
-  }
-}
